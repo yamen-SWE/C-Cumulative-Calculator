6d700f1470c02778e54ca288eb71f76fca136c02
Rant <yamen74653@gmail.com>
2026-05-07 17:57:48 +0300

    I added the remaining basic operations and made them more creative.

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int num1,num2,sum ;
-    printf("enter a chooes (1) : ");
-    scanf("%d",&num1);
-    printf("enetr a chooes (2) : ");
-    scanf("%d",&num2);
-    sum=num1+num2;
-    printf("the sum is %d\n",sum);;
+int main()
+{
+    float num1,num2;
+    float process;
+    char operationType;
+    printf("Enter a choice (1) : ");
+    scanf("%f",&num1);
+    printf("Enter an process (+ , - , * ,or /) : ");
+    scanf("  %c",&operationType);
+    printf("Enter a choice (2) : ");
+    scanf("%f",&num2);
+    switch(operationType){
+    case '+':
+        process = num1 + num2 ;
+        printf("The Sum is %.2f\n",process);
+        break;
+    case '-':
+        process = num1 - num2 ;
+        printf("The subtract is %.2f\n",process);
+        break;
+    case '*':
+        process = num1 * num2 ;
+        printf("The multiplication is %.2f\n",process);
+        break;
+    case '/':
+        if(num2 == 0){
+            printf("Please do not enter zero in the denominator.")
+        }else{
+        process = num1 / num2 ;
+        printf("The division is %.2f\n",process);
+        }
+        break;
+    default:
+        printf("%c : The operation is wrong\n",operationType);
+    }
     return 0;
 }
