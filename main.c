24615781afb72e81c49e5cb128f90fe902b7d975
Rant <yamen74653@gmail.com>
2026-05-15 22:17:16 +0300

    Update calculator to be cumulative and add error handling

--- main.c
+++ main.c
@@ -4,37 +4,42 @@
 int main()
 {
     float num1,num2;
-    float process;
     char operationType;
+    printf("");
     printf("Enter a choice (1) : ");
     scanf("%f",&num1);
     printf("Enter an process (+ , - , * ,or /) : ");
     scanf("  %c",&operationType);
-    printf("Enter a choice (2) : ");
-    scanf("%f",&num2);
-    switch(operationType){
-    case '+':
-        process = num1 + num2 ;
-        printf("The Sum is %.2f\n",process);
-        break;
-    case '-':
-        process = num1 - num2 ;
-        printf("The subtract is %.2f\n",process);
-        break;
-    case '*':
-        process = num1 * num2 ;
-        printf("The multiplication is %.2f\n",process);
-        break;
-    case '/':
-        if(num2 == 0){
-            printf("Please do not enter zero in the denominator.");
-        }else{
-            process = num1 / num2 ;
-            printf("The division is %.2f\n",process);
-        }
-        break;
-    default:
-        printf("%c : The operation is wrong\n",operationType);
+    int i = 2;
+    while(operationType != '!'){
+        printf("Enter a choice (%d) : ",i);
+        scanf("%f",&num2);
+        switch(operationType){
+            case '+':
+                num1 = num1 + num2 ;
+            break;
+            case '-':
+                num1 = num1 - num2 ;
+            break;
+            case '*':
+                num1 = num1 * num2 ;
+            break;
+            case '/':
+                if(num2 == 0){
+                    printf("Please do not enter zero in the denominator.\n");
+                }else{
+                    num1 = num1 / num2 ;
+                }
+            break;
+            default:
+                printf("%c : The operation is wrong\nDon't worry, I will ignore the incorrect signal and the last value you entered.\n",operationType);
+       }
+        printf("You can use the (!) symbol to end calculations.");
+        printf("Enter an process (+ , - , * ,or /) : ");
+        scanf("  %c",&operationType);
+        i++;
     }
+    printf("process : %.2f",num1);
+
     return 0;
 }
